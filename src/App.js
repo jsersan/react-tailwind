4c6a29d38b8d9458080b3a01d445345cd4645cbe
jsersan <jsersan@gmail.com>
2022-08-04 09:42:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
 
       {!isLoading && images.length === 0 
         && <h1 className="text-5xl text-center mx-auto mt-32">
-        No Images Found</h1> }
+        No hay imágenes</h1> }
 
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Cargando...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image} />
         ))}
