b5de7ba179407efe64f3a11c25f0d2cb0996144b
jsersan <jsersan@gmail.com>
2022-08-04 09:41:21 +0200

    Update ImageSearch.js

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -19,7 +19,7 @@ const ImageSearch = ({searchText}) => {
                     placeholder="Search Image Term..." />
                     <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 
                         hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
-                        Search
+                        Buscar
                     </button>
                 </div>
             </form>
